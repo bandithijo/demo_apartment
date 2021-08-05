bffa58cbf68389597823eacfc875ee25c75b903d
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-08-05 10:59:16 +0800

    Refactor constraints subdomain routes with Class

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
+class SubdomainConstraint
+  def self.matches?(request)
+    subdomains = Apartment::Elevators::Subdomain.excluded_subdomains
+    request.subdomain.present? && !subdomains.include?(request.subdomain)
+  end
+end
+
 Rails.application.routes.draw do
-  constraints subdomain: /^(?!www)(\w+)/ do
+  constraints SubdomainConstraint do
     resources :projects
   end
 
