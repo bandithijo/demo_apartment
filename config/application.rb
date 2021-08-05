182ae767f9db209d53b9956a711256f44f6e54da
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-08-05 10:44:08 +0800

    Remove config.middleware appartment from config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,6 @@ module DemoApartment
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.middleware.use Apartment::Elevators::Subdomain
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
